d6ab19ff4000fc69c53d0a94e07c446918e79eb1
Alexandra Nechita <alexandra.nechita@cgm.com>
2017-08-24 15:16:28 +0300

    Add main

---
+++ src/com/cgm/main/Main.java
@@ -0,0 +1,26 @@
+package com.cgm.main;
+
+import com.cgm.game.Card;
+import com.cgm.game.Deck;
+
+public class Main {
+	
+	public static void main(String[] args) 
+	{
+/**
+		Deck deck = new Deck();
+        for (Card card: deck.getDeck()) 
+        {
+        	System.out.println(card.toString());
+        }
+/**/
+		
+        System.out.println();
+        Deck d1 = new Deck();
+        for (Card card: d1.getDeck()) 
+        {
+        	System.out.println(card.toString());
+        }
+
+    }
+}
