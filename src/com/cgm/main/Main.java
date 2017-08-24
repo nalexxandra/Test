2402e0f669ab83dbe909862e308b4ff1c5e6b7a9
Alexandra Nechita <alexandra.nechita@cgm.com>
2017-08-24 15:26:03 +0300

    Main

--- src/com/cgm/main/Main.java
+++ src/com/cgm/main/Main.java
@@ -7,20 +7,13 @@ public class Main {
 	
 	public static void main(String[] args) 
 	{
-/**
+
 		Deck deck = new Deck();
         for (Card card: deck.getDeck()) 
         {
         	System.out.println(card.toString());
         }
-/**/
-		
-        System.out.println();
-        Deck d1 = new Deck();
-        for (Card card: d1.getDeck()) 
-        {
-        	System.out.println(card.toString());
-        }
+
 
     }
 }
